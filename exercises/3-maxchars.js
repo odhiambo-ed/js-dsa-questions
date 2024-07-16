d404dcbca7456025084b994fe9028372b6bfd6dd
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-07-16 19:36:20 +0300

    solve max char

--- exercises/3-maxchars.js
+++ exercises/3-maxchars.js
@@ -5,4 +5,20 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    let charObj = {};
+
+    for (let char of str) {
+        charObj[char] = charObj[char] + 1 || 1;
+    }
+
+    let max = 0;
+    let maxChar = '';
+    for (let char in charObj) {
+        if (charObj[char] > max) {
+            max = charObj[char];
+            maxChar = char;
+        }
+    }
+}
+console.log(maxChar('abcccccccd'));
