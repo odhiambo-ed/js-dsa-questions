c2a6bbc4bf39c437c8aa9c37533410897e05b88f
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-07-17 23:24:00 +0300

    solve array chunking

--- exercises/4-array-chunking.js
+++ exercises/4-array-chunking.js
@@ -8,4 +8,17 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+    newArray = [];
+    index = 0;
+
+    while (index < array.length) {
+        newArray.push(array.slice(index, index + size));
+        index += size;
+    } 
+
+    return newArray
+
+}
+
+console.log(chunk([1, 2, 3, 4], 3));
